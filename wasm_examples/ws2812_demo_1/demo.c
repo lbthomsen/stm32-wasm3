fb8f6451c76b5beade72c77ca29611c292f570d5
Lars Boegild Thomsen <lth@netcompartner.com>
2026-04-22 08:39:16 +0800

    Working again - fucked up something and can't find what it was

--- wasm_examples/ws2812_demo_1/demo.c
+++ wasm_examples/ws2812_demo_1/demo.c
@@ -4,19 +4,16 @@
 
 #include "api.h"
 
-void demo() {
 
-    uint32_t led = 0;
+void demo() {
+	uint32_t led = 0;
 	
 	for (;;) {
 		host_zero_led_values();
-		host_set_led(led, 0, 0, 20);
+		host_set_led(led, 10, 0, 10);
 		++led;
-		if (led >= 64) {
-			led = 0;
-		}
+		if (led >= 64) led = 0;
 		host_delay(20);
 	}
 }
 
-// vim: ts=4 et nowrap
