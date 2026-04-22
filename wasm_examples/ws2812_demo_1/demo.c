68d21e0454442be139d988875458b6fe1efd1e70
Lars Boegild Thomsen <lth@netcompartner.com>
2026-04-22 08:19:51 +0800

    Hmmmm

--- wasm_examples/ws2812_demo_1/demo.c
+++ wasm_examples/ws2812_demo_1/demo.c
@@ -5,27 +5,15 @@
 #include "api.h"
 
 void demo() {
+
     uint32_t led = 0;
-    uint8_t color = 0;
-    static const uint32_t led_color[][3] = {
-        {10, 0, 0}, 
-        {0, 10, 0}, 
-        {0, 0, 10}, 
-        {10, 10, 0}, 
-        {10, 0, 10}, 
-        {0, 10, 10}
-    };
 	
 	for (;;) {
 		host_zero_led_values();
-		host_set_led(led, led_color[color][0], led_color[color][1], led_color[color][2]);
+		host_set_led(led, 0, 0, 20);
 		++led;
 		if (led >= 64) {
 			led = 0;
-			++color;
-			if (color >= sizeof(led_color) / sizeof(led_color[0]) ) {
-				color = 0;
-			}
 		}
 		host_delay(20);
 	}
---
+++ wasm_examples/ws2812_demo_2/api.h
@@ -0,0 +1,9 @@
+
+#include <stdint.h>
+
+extern void host_delay(uint32_t ms); // Delay in ms ms
+extern void host_set_led(uint32_t led, uint32_t r, uint32_t g, uint32_t b);
+extern void host_zero_led_values();
+
+
+
